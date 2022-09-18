9089ad6b5f1bad42ef9b39bd87027184ae90ebd8
itselaras <gunadermawan62@gmail.com>
2022-09-18 21:21:45 +0700

    create face and eyes

--- app/src/main/java/com/gunder/likesapp/MainActivity.kt
+++ app/src/main/java/com/gunder/likesapp/MainActivity.kt
@@ -1,15 +1,55 @@
 package com.gunder.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.gunder.likesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
+    //    canvas
+    private val halfOfWidth = (mBitmap.width / 2).toFloat()
+    private val halfOfHeight = (mBitmap.height / 2).toFloat()
+    private val left = 150F
+    private val right = mBitmap.width - left
+    private val top = 250F
+    private val bottom = mBitmap.height.toFloat() - 50F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.ivContent.setImageBitmap(mBitmap)
+        showFace()
+        showEyes()
+        actionBar?.hide()
+    }
+
+    private fun showEyes() {
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        mCanvas.drawCircle(halfOfWidth - 100F, halfOfHeight - 10F, 50F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 100F, halfOfHeight - 10F, 50F, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.white, null)
+        mCanvas.drawCircle(halfOfWidth - 120F, halfOfHeight - 20F, 15F, mPaint)
+        mCanvas.drawCircle(halfOfWidth + 80F, halfOfHeight - 20F, 15F, mPaint)
+
+    }
+
+    private fun showFace() {
+        val face = RectF(left, top, right, bottom)
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.brown_right_hair, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
     }
 }
